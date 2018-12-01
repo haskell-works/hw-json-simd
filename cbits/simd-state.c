31a082393d6cf7bc3717023a5b203e58b933fd8e
John Ky <newhoggy@gmail.com>
2018-12-02 10:38:15 +1100

    Stuff

--- cbits/simd-state.c
+++ cbits/simd-state.c
@@ -323,38 +323,37 @@ void sm_process_chunk(
     uint32_t *out_phi_buffer) {  
   __m256i s = _mm256_set_epi64x(0, *inout_state, 0, *inout_state);
 
-  size_t in_length_div_8 = in_length / 8;
+  size_t in_length_part = in_length / 8;
 
   uint8_t *buf0 = in_buffer;
-  uint8_t *buf1 = in_buffer + in_length_div_8 * 1;
-  uint8_t *buf2 = in_buffer + in_length_div_8 * 2;
-  uint8_t *buf3 = in_buffer + in_length_div_8 * 3;
-  uint8_t *buf4 = in_buffer + in_length_div_8 * 4;
-  uint8_t *buf5 = in_buffer + in_length_div_8 * 5;
-  uint8_t *buf6 = in_buffer + in_length_div_8 * 6;
-  uint8_t *buf7 = in_buffer + in_length_div_8 * 7;
+  uint8_t *buf1 = in_buffer + in_length_part * 1;
+  uint8_t *buf2 = in_buffer + in_length_part * 2;
+  uint8_t *buf3 = in_buffer + in_length_part * 3;
+  uint8_t *buf4 = in_buffer + in_length_part * 4;
+  uint8_t *buf5 = in_buffer + in_length_part * 5;
+  uint8_t *buf6 = in_buffer + in_length_part * 6;
+  uint8_t *buf7 = in_buffer + in_length_part * 7;
 
-  uint32_t *phi0 = out_phi_buffer;
-  uint32_t *phi1 = out_phi_buffer + in_length_div_8 * 1;
-  uint32_t *phi2 = out_phi_buffer + in_length_div_8 * 2;
-  uint32_t *phi3 = out_phi_buffer + in_length_div_8 * 3;
+  // uint32_t *phi0 = out_phi_buffer;
+  // uint32_t *phi1 = out_phi_buffer + in_length_part * 1;
+  // uint32_t *phi2 = out_phi_buffer + in_length_part * 2;
+  // uint32_t *phi3 = out_phi_buffer + in_length_part * 3;
 
-  __m256i s0 = _mm256_set_epi64x(0, 0, 0, 0x03020100);
-  __m256i s1 = _mm256_set_epi64x(0, 0, 0, 0x03020100);
-  __m256i s2 = _mm256_set_epi64x(0, 0, 0, 0x03020100);
-  __m256i s3 = _mm256_set_epi64x(0, 0, 0, 0x03020100);
+  __m256i s0 = _mm256_set_epi64x(0x0f0e0d0c0b0a0908, 0x00706050403020100, 0x0f0e0d0c0b0a0908, 0x00706050403020100);
 
-  for (size_t i = 0; i < in_length_div_8; i += 1) {
-    s0 = _mm256_shuffle_epi8(_mm256_set_epi64x(0, transition_table_simd[buf4[i]], 0, transition_table_simd[buf0[i]]), s0);
-    s1 = _mm256_shuffle_epi8(_mm256_set_epi64x(0, transition_table_simd[buf5[i]], 0, transition_table_simd[buf1[i]]), s1);
-    s2 = _mm256_shuffle_epi8(_mm256_set_epi64x(0, transition_table_simd[buf6[i]], 0, transition_table_simd[buf2[i]]), s2);
-    s3 = _mm256_shuffle_epi8(_mm256_set_epi64x(0, transition_table_simd[buf7[i]], 0, transition_table_simd[buf3[i]]), s3);
+  for (size_t i = 0; i < in_length_part; i += 1) {
+    s = _mm256_shuffle_epi8(
+      _mm256_set_epi32(
+        transition_table_simd[buf7[i]],
+        transition_table_simd[buf6[i]],
+        transition_table_simd[buf5[i]],
+        transition_table_simd[buf4[i]],
+        transition_table_simd[buf3[i]],
+        transition_table_simd[buf2[i]],
+        transition_table_simd[buf1[i]],
+        transition_table_simd[buf0[i]]),
+      s);
   }
-
-  s = _mm256_shuffle_epi8(s0, s);
-  s = _mm256_shuffle_epi8(s1, s);
-  s = _mm256_shuffle_epi8(s2, s);
-  s = _mm256_shuffle_epi8(s3, s);
 
   *inout_state = _mm256_extract_epi64(s, 0);
 }
