c6a26cc31f6e99b2274aeb835720efd6a17ca68b
John Ky <newhoggy@gmail.com>
2018-12-07 19:08:41 +1100

    Fix build

--- cbits/debug.h
+++ cbits/debug.h
@@ -1,6 +1,7 @@
-#include <stdio.h>
 #include <immintrin.h>
 #include <mmintrin.h>
+#include <stdint.h>
+#include <stdio.h>
 
 void print256_num(__m256i var);
 
@@ -10,14 +11,54 @@ void fprint256_num(FILE *file, __m256i var);
 
 void fprint128_num(FILE *file, __m128i var);
 
-void print_bits_8(uint8_t v);
+inline void print_bits_8(uint8_t v) {
+  char *digits = "01";
 
-void print_bits_16(uint16_t v);
+  for (int i = 0; i < 8; ++i) {
+    printf("%c", digits[(v >> i) & 1]);
+  }
+}
 
-void print_bits_32(uint32_t v);
+inline void print_bits_16(uint16_t v) {
+  char *digits = "01";
 
-void print_bits_64(uint64_t v);
+  for (int i = 0; i < 16; ++i) {
+    printf("%c", digits[(v >> i) & 1]);
+  }
+}
 
-void print_bits_128(__m128i v);
+inline void print_bits_32(uint32_t v) {
+  char *digits = "01";
 
-void print_bits_256(__m256i v);
+  for (int i = 0; i < 32; ++i) {
+    printf("%c", digits[(v >> i) & 1]);
+  }
+}
+
+inline void print_bits_64(uint64_t v) {
+  char *digits = "01";
+
+  for (int i = 0; i < 64; ++i) {
+    printf("%c", digits[(v >> i) & 1]);
+  }
+}
+
+inline void print_bits_128(__m128i v) {
+  for (int i = 0; i < 2; ++i) {
+    if (i > 0) {
+      printf("-");
+    }
+
+    print_bits_64(_mm_extract_epi64(v, i));
+  }
+}
+
+inline void print_bits_256(__m256i v) {
+  for (int i = 0; i < 4; ++i) {
+    if (i > 0) {
+      printf("-");
+    }
+
+    print_bits_64(_mm256_extract_epi64(v, i));
+  }
+}
--- project.sh
+++ project.sh
@@ -1,47 +1,19 @@
 #!/usr/bin/env bash
 
-STACK_FLAGS="
-  --flag bits-extra:bmi2
-  --flag hw-rankselect-base:bmi2
-  --flag hw-rankselect:bmi2
-  --flag hw-json-simd:bmi2
-  --flag hw-json-simd:avx2
-"
+CABAL_FLAGS="-j8"
 
 cmd="$1"
 
 shift
 
 case "$cmd" in
-  install)
-    stack install \
-      --test --no-run-tests --bench --no-run-benchmarks \
-      $STACK_FLAGS "$@"
-    ;;
-
   build)
-    stack build \
-      --test --no-run-tests --bench --no-run-benchmarks \
-      $STACK_FLAGS "$@"
+    cabal new-build \
+      $CABAL_FLAGS "$@"
     ;;
 
   test)
-    stack test \
-      $STACK_FLAGS "$@"
-    ;;
-
-  exec)
-    stack exec \
-      $STACK_FLAGS "$@"
-    ;;
-
-  bench)
-    stack bench \
-      $STACK_FLAGS "$@"
-    ;;
-
-  repl)
-    stack repl \
-      $STACK_FLAGS "$@"
+    cabal new-test \
+      $CABAL_FLAGS "$@"
     ;;
 esac
